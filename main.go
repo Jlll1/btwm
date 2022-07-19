380821dd6866aa1d360eed1a60c48cfa4df5dc10
Jlll1 <arghantentua@tutanota.com>
2022-07-19 21:14:07 +0200

    Handle ConfigureRequest and MapNotify

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 
@@ -8,11 +9,42 @@ import (
 	"github.com/jezek/xgb/xproto"
 )
 
+func HandleConfigureRequest(ev xproto.ConfigureRequestEvent, conn *xgb.Conn) {
+	fmt.Println("configure")
+	configureEvent := xproto.ConfigureNotifyEvent{
+		Event:            ev.Window,
+		Window:           ev.Window,
+		AboveSibling:     0,
+		X:                ev.X,
+		Y:                ev.Y,
+		Width:            ev.Width,
+		Height:           ev.Height,
+		BorderWidth:      ev.BorderWidth,
+		OverrideRedirect: false,
+	}
+	xproto.SendEventChecked(
+		conn, false, ev.Window, xproto.EventMaskStructureNotify, string(configureEvent.Bytes()))
+}
+
 func HandleKeyPress(ev xproto.KeyPressEvent) {
 	// 'p'
 	if ev.Detail == 33 {
 		exec.Command("dmenu_run").Run()
 	}
+}
+
+func HandleMapNotify(ev xproto.MapNotifyEvent, conn *xgb.Conn, screenWidth uint16, screenHeight uint16) {
+	fmt.Println("Map")
+	attributes, err := xproto.GetWindowAttributes(conn, ev.Window).Reply()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if attributes.OverrideRedirect {
+		return
+	}
+
+	values := []uint32{0, 0, uint32(screenWidth), uint32(screenHeight)}
+	xproto.ChangeWindowAttributesChecked(conn, ev.Window, xproto.CwEventMask, values).Check()
 }
 
 func main() {
@@ -26,7 +58,8 @@ func main() {
 		log.Fatal("couldn't parse connection info")
 	}
 
-	root := connInfo.DefaultScreen(conn).Root
+	screen := connInfo.DefaultScreen(conn)
+	root := screen.Root
 
 	mask := []uint32{
 		xproto.EventMaskKeyPress |
@@ -49,9 +82,13 @@ func main() {
 			break
 		}
 
-		switch ev.(type) {
+		switch event := ev.(type) {
 		case xproto.KeyPressEvent:
-			HandleKeyPress(ev.(xproto.KeyPressEvent))
+			HandleKeyPress(event)
+		case xproto.ConfigureRequestEvent:
+			HandleConfigureRequest(event, conn)
+		case xproto.MapNotifyEvent:
+			HandleMapNotify(event, conn, screen.WidthInPixels, screen.HeightInPixels)
 		}
 	}
 }
